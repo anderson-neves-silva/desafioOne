520baf0e63bbe38d63e22ede6bd76daa0b438e07
Anderson Neves <andersonneves_@hotmail.com>
2023-09-19 16:31:56 -0300

    alterando para corrigir o texto para o amigo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Opaaa blz queridão sou eu o Anderson quem escreveu aqui nessas poucas palavras mas é de coração, olha hoje vim aqui agradecer por sua amizade, eu não tenho muitos amigos mas hoje eu verdadeiramente posso afirmar que você é meu amigo de verdade, obrigado por ser meu amigo, continue sempre sendo essa cara bacana que você é, conte sempre comigo my brother…</h1>
+  return <h1>Opaaa blz queridão sou eu o Anderson quem escreveu aqui essas poucas palavras mas saiba que é de coração, olha hoje vim aqui agradecer por sua amizade, eu não tenho muitos amigos mas hoje eu verdadeiramente posso afirmar que você é meu amigo de verdade, obrigado por ser meu amigo, continue sempre sendo esse cara bacana que você é, conte sempre comigo my brother…</h1>
 }
 
 export default Home;
